a4c8485d9a4b014c010c907af7f3f65b1016e538
Jeremias Marinho <jeremias10@msn.com>
2021-10-25 10:35:01 -0300

    Array de negociações

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,20 +1,27 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: Number;
-  private _valor: Number;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-  constructor(data: Date, quantidade: Number, valor: Number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
-  get data(){
-    return this._data;
-  }
-  get quantidade(){
-    return this._quantidade;
-  }
-  get valor(){
-    return this._valor;
-  }
+    constructor(data: Date, quantidade: number, valor: number) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data(): Date {
+        return this._data;
+    }
+
+    get quantidade(): number {
+        return this._quantidade;
+    }
+
+    get valor(): number {
+        return this._valor;
+    }
+
+    get volume(): number {
+        return this._quantidade * this._valor;
+    }
 }
